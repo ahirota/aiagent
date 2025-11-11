95146c7472716395b48bf8af0abd3da3b2e2e67d
ahirota <avjhirota@gmail.com>
2025-11-10 21:51:28 -0800

    [AIAGENT_2_3] Add Get File Content Utility

--- tests.py
+++ tests.py
@@ -1,31 +1,58 @@
-from functions.get_files_info import get_files_info
+from functions.get_files_info import get_files_info, get_file_content
 
 
-def test_case_1():
-    response = get_files_info("calculator", ".")
-    print("Result for current directory:")
+# def test_get_files_info_1():
+#     response = get_files_info("calculator", ".")
+#     print("Result for current directory:")
+#     print(response)
+
+# def test_get_files_info_2():
+#     response = get_files_info("calculator", "pkg")
+#     print("Result for 'pkg' directory:")
+#     print(response)
+
+# def test_get_files_info_3():
+#     response = get_files_info("calculator", "/bin")
+#     print("Result for '/bin' directory:")
+#     print(response)
+
+# def test_get_files_info_4():
+#     response = get_files_info("calculator", "../")
+#     print("Result for '../' directory:")
+#     print(response)
+
+# def test_get_files_content_truncation():
+#     response = get_file_content("calculator", "lorem.txt")
+#     print(response)
+
+def test_get_files_content_1():
+    response = get_file_content("calculator", "main.py")
     print(response)
 
-def test_case_2():
-    response = get_files_info("calculator", "pkg")
-    print("Result for 'pkg' directory:")
+def test_get_files_content_2():
+    response = get_file_content("calculator", "pkg/calculator.py")
     print(response)
 
-def test_case_3():
-    response = get_files_info("calculator", "/bin")
-    print("Result for '/bin' directory:")
+def test_get_files_content_3():
+    response = get_file_content("calculator", "/bin/cat")
     print(response)
 
-def test_case_4():
-    response = get_files_info("calculator", "../")
-    print("Result for '../' directory:")
+def test_get_files_content_4():
+    response = get_file_content("calculator", "pkg/does_not_exist.py")
     print(response)
 
 def main():
-    test_case_1()
-    test_case_2()
-    test_case_3()
-    test_case_4()
+    # test_get_files_info_1()
+    # test_get_files_info_2()
+    # test_get_files_info_3()
+    # test_get_files_info_4()
+
+    # test_get_files_content_truncation()
+
+    test_get_files_content_1()
+    test_get_files_content_2()
+    test_get_files_content_3()
+    test_get_files_content_4()
 
 if __name__ == "__main__":
     main()
